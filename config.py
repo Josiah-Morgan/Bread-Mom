5b538b898fdbc29748d4765a14688b058d91b00b
Josiah Morgan <123709411+Josiah-Morgan@users.noreply.github.com>
2024-04-08 13:55:19 -0400

    Create config.py
    config

---
+++ config.py
@@ -0,0 +1,45 @@
+# Config
+
+# AutoMod
+BLACK_LISTED_WORDS = [
+    "discord.gg/",
+    "discord.com/invite",
+    "discordapp.com/invite",
+    "discord.com/oauth2/authorize",
+    "discordapp.com/oauth2/authorize",
+    "top.gg/",
+    "privateserverlinkcode=",
+    "nigger",
+    "nigga",
+    "kys",
+    "killyourself",
+]
+WHITE_LISTED = [432689076626522132, 730594098695635014]
+
+# OnJoin
+# Servers
+BREAD_KINGDOM_ID = 731297050880376832
+BREAD_TEMPLATES_ID = 837533258907385887
+# Roles
+BREAD_KINGDOM_JOIN_ROLES = [
+    761273717174173778,
+    761273928395653130,
+    758514759430832138,
+    761272650827431952,
+]  # staff, roles, bread bro, pings
+BREAD_TEMPLATES_JOIN_ROLES = [845300815819440139]
+# Channels
+LOG_CHANNEL = 899738004187402301
+CHAT_CHANNEL = 757043848135311390  # welcome
+FAQ_CHANNEL = 914694600407515207
+SUPPORT_CHANNEL = 947473453299159040
+# Text
+BREAD_WINNER_B_EMOJI = "<:BreadWinnerB:894408972063346718>"
+WELCOME_MESSAGE = f"Try out the bot for the server with **`/`**\nHere for **support?** Then look at <#{FAQ_CHANNEL}> or go to <#{SUPPORT_CHANNEL}>"
+
+# FAQ
+SIGNING_VIDEO = "https://youtu.be/GIic9B7QIXQ"
+TO_LOW_IMAGE = "https://cdn.discordapp.com/attachments/757043848789753886/774475065969999892/sketch-1604719627791.png"
+NEEDS_MANAGE_ROLES = "https://www.youtube.com/shorts/xHX1vNNHSfg"
+COMMON_SIGNING_ERRORS = f"Try checking the following:\n 1. Is the bot able to add roles to your players? \n 2. Is the bot able to send messages in that channel?\n{TO_LOW_IMAGE}\n{NEEDS_MANAGE_ROLES}"
+LINKS_CHANNEL = 937165662512623617
